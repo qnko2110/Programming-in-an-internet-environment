200f94692dc71566d4e2447357639a3190a67e4f
qnko2110 <147039344+qnko2110@users.noreply.github.com>
2025-09-30 15:45:26 +0300

    Add files via upload

---
+++ ScanPortWithThreadPool.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scanportwiththreadpool;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/**
+ *
+ * @author User
+ */
+public class ScanPortWithThreadPool {
+final static int TIMEOUT = 2500;
+static final int START_PORT = 1;
+static final int END_PORT = 1024;
+static final String HOST_NAME = "www.tugab.bg";
+static Socket socket;
+static InetAddress ip;
+final static int NUM_OF_THREADS =1024;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+ip = InetAddress.getByName(HOST_NAME);
+} catch (UnknownHostException uhs) {
+System.out.println("Невалидно име на хост.");
+return;
+}
+
+ //ExecutorService pool = Executors.newFixedThreadPool(NUM_OF_THREADS);
+ExecutorService pool = Executors.newCachedThreadPool();
+for (int i = START_PORT; i < END_PORT; i++) {
+pool.execute(new ScanJob(i));
+}
+
+ pool.shutdown();
+}
+
+  static class ScanJob implements Runnable {
+
+ private Socket socket;
+int port;
+
+ public ScanJob(int port) {
+this.port = port;
+}
+
+ @Override
+public void run() {
+
+ try {
+
+ Socket socket = new Socket();
+socket.connect(new InetSocketAddress(ip, port), TIMEOUT);
+System.out.println(port);
+
+ } catch (IOException ex) {
+
+ } finally {
+
+ socket = null;
+
+ }
+}     
+    }
+    
+}
